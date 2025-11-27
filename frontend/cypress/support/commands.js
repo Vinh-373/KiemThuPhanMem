ab0bffdb5fa5982ff4e23215a79bc6a4b4ac5a7a
tvthuong290605 <tvthuong290605@gmail.com>
2025-11-27 20:23:51 +0700

    Update commands.js

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -2,7 +2,7 @@
 
 Cypress.Commands.add('login', (username = 'testuser', password = 'Test123') => {
   cy.visit('/login');
-  cy.get('input[name=username]').type(username);
+  cy.get('input[name=username]').type(text);
   cy.get('input[name=password]').type(password);
   cy.get('button[type=submit]').click();
 });
