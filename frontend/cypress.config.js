01a05ffb71fdcbba682881e146d2b36dd923d966
tvthuong290605 <tvthuong290605@gmail.com>
2025-11-30 00:56:29 +0700

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,12 +1,11 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     viewportWidth: 1280,
     viewportHeight: 720,
 
-    
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
     fixturesFolder: "cypress/fixtures",
