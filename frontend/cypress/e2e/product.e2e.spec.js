76a651313cbda5283f357cd87406df25b174ce2b
tvthuong290605 <tvthuong290605@gmail.com>
2025-11-30 03:37:29 +0700

    Update product.e2e.spec.js

--- frontend/cypress/e2e/product.e2e.spec.js
+++ frontend/cypress/e2e/product.e2e.spec.js
@@ -1,18 +1,36 @@
 import ProductPage from '../pages/ProductPage';
 
 describe('Product E2E Tests', () => {
-  const productPage = new ProductPage();
+  let productPage;
 
   beforeEach(() => {
     cy.login('testuser', 'Test123');
+    productPage = new ProductPage();
     productPage.visit();
-  });
-
-  it('Hiển thị grid sản phẩm', () => {
-    productPage.getGrid().should('exist');
   });
 
   it('Hiển thị thông báo khi không có sản phẩm', () => {
     cy.contains('No products available').should('exist');
+  });
+
+  it('Có thể điền và submit form sản phẩm', () => {
+    productPage.getForm().should('exist');
+    productPage.fillProductForm({
+      name: 'Laptop Dell',
+      price: '15000000',
+      quantity: '10'
+    });
+    productPage.getForm().submit();
+
+    productPage.getSuccessMessage().should('exist');
+  });
+
+  it('Hiển thị sản phẩm trong danh sách', () => {
+    productPage.getProductInList('Laptop Dell').should('exist');
+  });
+
+  it('Hiển thị chi tiết sản phẩm', () => {
+    productPage.getProductName().should('exist');
+    productPage.getProductPrice().should('contain', 'VNĐ');
   });
 });
