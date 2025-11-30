85e45e8bf03d1bb8b97a8473dc50fd595ba171e6
tvthuong290605 <tvthuong290605@gmail.com>
2025-12-01 00:08:16 +0700

    Update jest.setup.js

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -2,3 +2,10 @@ import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({}),
+  })
+);
