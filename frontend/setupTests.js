39a97885877d2a9248c359253c0a2b30264b904f
tvthuong290605 <tvthuong290605@gmail.com>
2025-12-01 00:12:17 +0700

    Update setupTests.js

--- frontend/setupTests.js
+++ frontend/setupTests.js
@@ -1,2 +1,8 @@
 import "@testing-library/jest-dom";
 require('jest-fetch-mock').enableMocks();
+
+// 👇 FIX lỗi TextEncoder không tồn tại trong Jest
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
